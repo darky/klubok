e428b09cd89d3bfdbf71fe3f7eb7f6ca9478ebbe
Vladislav Botvin <darkvlados@gmail.com>
2024-11-01 20:08:55 +0300

    klubok for arity 3-10 too

--- index.ts
+++ index.ts
@@ -18,6 +18,413 @@ export function klubok<K1 extends string, K2 extends string, C extends object, R
   ctx: C & { [k in K1]?: R1 } & { [k in K2]?: R2 },
   only?: Set<K1 | K2>
 ) => Promise<C & { [k in K1]: R1 } & { [k in K2]: R2 }>
+export function klubok<K1 extends string, K2 extends string, K3 extends string, C extends object, R1, R2, R3>(
+  fn1: KeyedFunction<K1, (ctx: C) => Promise<R1> | R1>,
+  fn2: KeyedFunction<K2, (ctx: C & { [k in K1]: R1 }) => Promise<R2> | R2>,
+  fn3: KeyedFunction<K3, (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 }) => Promise<R3> | R3>
+): (
+  ctx: C & { [k in K1]?: R1 } & { [k in K2]?: R2 } & { [k in K3]?: R3 },
+  only?: Set<K1 | K2 | K3>
+) => Promise<C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 }>
+export function klubok<
+  K1 extends string,
+  K2 extends string,
+  K3 extends string,
+  K4 extends string,
+  C extends object,
+  R1,
+  R2,
+  R3,
+  R4
+>(
+  fn1: KeyedFunction<K1, (ctx: C) => Promise<R1> | R1>,
+  fn2: KeyedFunction<K2, (ctx: C & { [k in K1]: R1 }) => Promise<R2> | R2>,
+  fn3: KeyedFunction<K3, (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 }) => Promise<R3> | R3>,
+  fn4: KeyedFunction<K4, (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 }) => Promise<R4> | R4>
+): (
+  ctx: C & { [k in K1]?: R1 } & { [k in K2]?: R2 } & { [k in K3]?: R3 } & { [k in K4]?: R4 },
+  only?: Set<K1 | K2 | K3 | K4>
+) => Promise<C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 }>
+export function klubok<
+  K1 extends string,
+  K2 extends string,
+  K3 extends string,
+  K4 extends string,
+  K5 extends string,
+  C extends object,
+  R1,
+  R2,
+  R3,
+  R4,
+  R5
+>(
+  fn1: KeyedFunction<K1, (ctx: C) => Promise<R1> | R1>,
+  fn2: KeyedFunction<K2, (ctx: C & { [k in K1]: R1 }) => Promise<R2> | R2>,
+  fn3: KeyedFunction<K3, (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 }) => Promise<R3> | R3>,
+  fn4: KeyedFunction<K4, (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 }) => Promise<R4> | R4>,
+  fn5: KeyedFunction<
+    K5,
+    (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 }) => Promise<R5> | R5
+  >
+): (
+  ctx: C & { [k in K1]?: R1 } & { [k in K2]?: R2 } & { [k in K3]?: R3 } & { [k in K4]?: R4 } & { [k in K5]?: R5 },
+  only?: Set<K1 | K2 | K3 | K4 | K5>
+) => Promise<C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 }>
+export function klubok<
+  K1 extends string,
+  K2 extends string,
+  K3 extends string,
+  K4 extends string,
+  K5 extends string,
+  K6 extends string,
+  C extends object,
+  R1,
+  R2,
+  R3,
+  R4,
+  R5,
+  R6
+>(
+  fn1: KeyedFunction<K1, (ctx: C) => Promise<R1> | R1>,
+  fn2: KeyedFunction<K2, (ctx: C & { [k in K1]: R1 }) => Promise<R2> | R2>,
+  fn3: KeyedFunction<K3, (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 }) => Promise<R3> | R3>,
+  fn4: KeyedFunction<K4, (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 }) => Promise<R4> | R4>,
+  fn5: KeyedFunction<
+    K5,
+    (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 }) => Promise<R5> | R5
+  >,
+  fn6: KeyedFunction<
+    K6,
+    (
+      ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 }
+    ) => Promise<R6> | R6
+  >
+): (
+  ctx: C & { [k in K1]?: R1 } & { [k in K2]?: R2 } & { [k in K3]?: R3 } & { [k in K4]?: R4 } & { [k in K5]?: R5 } & {
+    [k in K6]?: R6
+  },
+  only?: Set<K1 | K2 | K3 | K4 | K5 | K6>
+) => Promise<
+  C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 } & {
+    [k in K6]: R6
+  }
+>
+export function klubok<
+  K1 extends string,
+  K2 extends string,
+  K3 extends string,
+  K4 extends string,
+  K5 extends string,
+  K6 extends string,
+  K7 extends string,
+  C extends object,
+  R1,
+  R2,
+  R3,
+  R4,
+  R5,
+  R6,
+  R7
+>(
+  fn1: KeyedFunction<K1, (ctx: C) => Promise<R1> | R1>,
+  fn2: KeyedFunction<K2, (ctx: C & { [k in K1]: R1 }) => Promise<R2> | R2>,
+  fn3: KeyedFunction<K3, (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 }) => Promise<R3> | R3>,
+  fn4: KeyedFunction<K4, (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 }) => Promise<R4> | R4>,
+  fn5: KeyedFunction<
+    K5,
+    (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 }) => Promise<R5> | R5
+  >,
+  fn6: KeyedFunction<
+    K6,
+    (
+      ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 }
+    ) => Promise<R6> | R6
+  >,
+  fn7: KeyedFunction<
+    K7,
+    (
+      ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 } & {
+        [k in K6]: R6
+      }
+    ) => Promise<R7> | R7
+  >
+): (
+  ctx: C & { [k in K1]?: R1 } & { [k in K2]?: R2 } & { [k in K3]?: R3 } & { [k in K4]?: R4 } & { [k in K5]?: R5 } & {
+    [k in K6]?: R6
+  } & {
+    [k in K7]?: R7
+  },
+  only?: Set<K1 | K2 | K3 | K4 | K5 | K6 | K7>
+) => Promise<
+  C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 } & {
+    [k in K6]: R6
+  } & {
+    [k in K7]: R7
+  }
+>
+export function klubok<
+  K1 extends string,
+  K2 extends string,
+  K3 extends string,
+  K4 extends string,
+  K5 extends string,
+  K6 extends string,
+  K7 extends string,
+  K8 extends string,
+  C extends object,
+  R1,
+  R2,
+  R3,
+  R4,
+  R5,
+  R6,
+  R7,
+  R8
+>(
+  fn1: KeyedFunction<K1, (ctx: C) => Promise<R1> | R1>,
+  fn2: KeyedFunction<K2, (ctx: C & { [k in K1]: R1 }) => Promise<R2> | R2>,
+  fn3: KeyedFunction<K3, (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 }) => Promise<R3> | R3>,
+  fn4: KeyedFunction<K4, (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 }) => Promise<R4> | R4>,
+  fn5: KeyedFunction<
+    K5,
+    (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 }) => Promise<R5> | R5
+  >,
+  fn6: KeyedFunction<
+    K6,
+    (
+      ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 }
+    ) => Promise<R6> | R6
+  >,
+  fn7: KeyedFunction<
+    K7,
+    (
+      ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 } & {
+        [k in K6]: R6
+      }
+    ) => Promise<R7> | R7
+  >,
+  fn8: KeyedFunction<
+    K8,
+    (
+      ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 } & {
+        [k in K6]: R6
+      } & {
+        [k in K7]: R7
+      }
+    ) => Promise<R8> | R8
+  >
+): (
+  ctx: C & { [k in K1]?: R1 } & { [k in K2]?: R2 } & { [k in K3]?: R3 } & { [k in K4]?: R4 } & { [k in K5]?: R5 } & {
+    [k in K6]?: R6
+  } & {
+    [k in K7]?: R7
+  } & {
+    [k in K8]?: R8
+  },
+  only?: Set<K1 | K2 | K3 | K4 | K5 | K6 | K7 | K8>
+) => Promise<
+  C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 } & {
+    [k in K6]: R6
+  } & {
+    [k in K7]: R7
+  } & {
+    [k in K8]: R8
+  }
+>
+export function klubok<
+  K1 extends string,
+  K2 extends string,
+  K3 extends string,
+  K4 extends string,
+  K5 extends string,
+  K6 extends string,
+  K7 extends string,
+  K8 extends string,
+  K9 extends string,
+  C extends object,
+  R1,
+  R2,
+  R3,
+  R4,
+  R5,
+  R6,
+  R7,
+  R8,
+  R9
+>(
+  fn1: KeyedFunction<K1, (ctx: C) => Promise<R1> | R1>,
+  fn2: KeyedFunction<K2, (ctx: C & { [k in K1]: R1 }) => Promise<R2> | R2>,
+  fn3: KeyedFunction<K3, (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 }) => Promise<R3> | R3>,
+  fn4: KeyedFunction<K4, (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 }) => Promise<R4> | R4>,
+  fn5: KeyedFunction<
+    K5,
+    (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 }) => Promise<R5> | R5
+  >,
+  fn6: KeyedFunction<
+    K6,
+    (
+      ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 }
+    ) => Promise<R6> | R6
+  >,
+  fn7: KeyedFunction<
+    K7,
+    (
+      ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 } & {
+        [k in K6]: R6
+      }
+    ) => Promise<R7> | R7
+  >,
+  fn8: KeyedFunction<
+    K8,
+    (
+      ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 } & {
+        [k in K6]: R6
+      } & {
+        [k in K7]: R7
+      }
+    ) => Promise<R8> | R8
+  >,
+  fn9: KeyedFunction<
+    K9,
+    (
+      ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 } & {
+        [k in K6]: R6
+      } & {
+        [k in K7]: R7
+      } & {
+        [k in K8]: R8
+      }
+    ) => Promise<R9> | R9
+  >
+): (
+  ctx: C & { [k in K1]?: R1 } & { [k in K2]?: R2 } & { [k in K3]?: R3 } & { [k in K4]?: R4 } & { [k in K5]?: R5 } & {
+    [k in K6]?: R6
+  } & {
+    [k in K7]?: R7
+  } & {
+    [k in K8]?: R8
+  } & {
+    [k in K9]?: R9
+  },
+  only?: Set<K1 | K2 | K3 | K4 | K5 | K6 | K7 | K8 | K9>
+) => Promise<
+  C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 } & {
+    [k in K6]: R6
+  } & {
+    [k in K7]: R7
+  } & {
+    [k in K8]: R8
+  } & {
+    [k in K9]: R9
+  }
+>
+export function klubok<
+  K1 extends string,
+  K2 extends string,
+  K3 extends string,
+  K4 extends string,
+  K5 extends string,
+  K6 extends string,
+  K7 extends string,
+  K8 extends string,
+  K9 extends string,
+  K10 extends string,
+  C extends object,
+  R1,
+  R2,
+  R3,
+  R4,
+  R5,
+  R6,
+  R7,
+  R8,
+  R9,
+  R10
+>(
+  fn1: KeyedFunction<K1, (ctx: C) => Promise<R1> | R1>,
+  fn2: KeyedFunction<K2, (ctx: C & { [k in K1]: R1 }) => Promise<R2> | R2>,
+  fn3: KeyedFunction<K3, (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 }) => Promise<R3> | R3>,
+  fn4: KeyedFunction<K4, (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 }) => Promise<R4> | R4>,
+  fn5: KeyedFunction<
+    K5,
+    (ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 }) => Promise<R5> | R5
+  >,
+  fn6: KeyedFunction<
+    K6,
+    (
+      ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 }
+    ) => Promise<R6> | R6
+  >,
+  fn7: KeyedFunction<
+    K7,
+    (
+      ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 } & {
+        [k in K6]: R6
+      }
+    ) => Promise<R7> | R7
+  >,
+  fn8: KeyedFunction<
+    K8,
+    (
+      ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 } & {
+        [k in K6]: R6
+      } & {
+        [k in K7]: R7
+      }
+    ) => Promise<R8> | R8
+  >,
+  fn9: KeyedFunction<
+    K9,
+    (
+      ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 } & {
+        [k in K6]: R6
+      } & {
+        [k in K7]: R7
+      } & {
+        [k in K8]: R8
+      }
+    ) => Promise<R9> | R9
+  >,
+  fn10: KeyedFunction<
+    K10,
+    (
+      ctx: C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 } & {
+        [k in K6]: R6
+      } & {
+        [k in K7]: R7
+      } & {
+        [k in K8]: R8
+      } & {
+        [k in K9]: R9
+      }
+    ) => Promise<R10> | R10
+  >
+): (
+  ctx: C & { [k in K1]?: R1 } & { [k in K2]?: R2 } & { [k in K3]?: R3 } & { [k in K4]?: R4 } & { [k in K5]?: R5 } & {
+    [k in K6]?: R6
+  } & {
+    [k in K7]?: R7
+  } & {
+    [k in K8]?: R8
+  } & {
+    [k in K9]?: R9
+  } & {
+    [k in K10]?: R10
+  },
+  only?: Set<K1 | K2 | K3 | K4 | K5 | K6 | K7 | K8 | K9 | K10>
+) => Promise<
+  C & { [k in K1]: R1 } & { [k in K2]: R2 } & { [k in K3]: R3 } & { [k in K4]: R4 } & { [k in K5]: R5 } & {
+    [k in K6]: R6
+  } & {
+    [k in K7]: R7
+  } & {
+    [k in K8]: R8
+  } & {
+    [k in K9]: R9
+  } & {
+    [k in K10]: R10
+  }
+>
 export function klubok(...fns: KeyedFunction<string, Function>[]) {
   return (rootCtx = {}, only = new Set<string>()) =>
     fns.reduce(
--- test.ts
+++ test.ts
@@ -4,7 +4,9 @@ import assert from 'node:assert'
 
 test('1 param sync, resp', async () => {
   const fn = klubok(pure('incNumber', (ctx: { number: number }) => ctx.number + 1))
-  assert.deepStrictEqual(await fn({ number: 1 }), { number: 1, incNumber: 2 })
+  const resp = await fn({ number: 1 })
+  assert.strictEqual(resp.number, 1)
+  assert.strictEqual(resp.incNumber, 2)
 })
 
 test('1 param async, resp', async () => {
@@ -62,5 +64,162 @@ test('2 params async, only', async () => {
   assert.deepStrictEqual(await fn({ number: 1 }, new Set(['incNumber'])), {
     number: 1,
     incNumber: 2,
+  })
+})
+
+test('3 params sync, resp', async () => {
+  const fn = klubok(
+    pure('incNumber', (ctx: { number: number }) => ctx.number + 1),
+    pure('strNumber', ({ incNumber }) => incNumber.toString()),
+    pure('strLength', ({ strNumber }) => strNumber.length)
+  )
+  assert.deepStrictEqual(await fn({ number: 1 }), { number: 1, incNumber: 2, strNumber: '2', strLength: 1 })
+})
+
+test('4 params sync, resp', async () => {
+  const fn = klubok(
+    pure('incNumber', (ctx: { number: number }) => ctx.number + 1),
+    pure('strNumber', ({ incNumber }) => incNumber.toString()),
+    pure('strLength', ({ strNumber }) => strNumber.length),
+    pure('strLengthPositive', ({ strLength }) => strLength > 0)
+  )
+  assert.deepStrictEqual(await fn({ number: 1 }), {
+    number: 1,
+    incNumber: 2,
+    strNumber: '2',
+    strLength: 1,
+    strLengthPositive: true,
+  })
+})
+
+test('5 params sync, resp', async () => {
+  const fn = klubok(
+    pure('incNumber', (ctx: { number: number }) => ctx.number + 1),
+    pure('strNumber', ({ incNumber }) => incNumber.toString()),
+    pure('strLength', ({ strNumber }) => strNumber.length),
+    pure('strLengthPositive', ({ strLength }) => strLength > 0),
+    pure('sum', ({ strLength, incNumber }) => strLength + incNumber)
+  )
+  assert.deepStrictEqual(await fn({ number: 1 }), {
+    number: 1,
+    incNumber: 2,
+    strNumber: '2',
+    strLength: 1,
+    strLengthPositive: true,
+    sum: 3,
+  })
+})
+
+test('6 params sync, resp', async () => {
+  const fn = klubok(
+    pure('incNumber', (ctx: { number: number }) => ctx.number + 1),
+    pure('strNumber', ({ incNumber }) => incNumber.toString()),
+    pure('strLength', ({ strNumber }) => strNumber.length),
+    pure('strLengthPositive', ({ strLength }) => strLength > 0),
+    pure('sum', ({ strLength, incNumber }) => strLength + incNumber),
+    pure('numbersArray', ({ number, incNumber, strLength, sum }) => [number, incNumber, strLength, sum])
+  )
+  assert.deepStrictEqual(await fn({ number: 1 }), {
+    number: 1,
+    incNumber: 2,
+    strNumber: '2',
+    strLength: 1,
+    strLengthPositive: true,
+    sum: 3,
+    numbersArray: [1, 2, 1, 3],
+  })
+})
+
+test('7 params sync, resp', async () => {
+  const fn = klubok(
+    pure('incNumber', (ctx: { number: number }) => ctx.number + 1),
+    pure('strNumber', ({ incNumber }) => incNumber.toString()),
+    pure('strLength', ({ strNumber }) => strNumber.length),
+    pure('strLengthPositive', ({ strLength }) => strLength > 0),
+    pure('sum', ({ strLength, incNumber }) => strLength + incNumber),
+    pure('numbersArray', ({ number, incNumber, strLength, sum }) => [number, incNumber, strLength, sum]),
+    pure('arrLength', ({ numbersArray }) => numbersArray.length)
+  )
+  assert.deepStrictEqual(await fn({ number: 1 }), {
+    number: 1,
+    incNumber: 2,
+    strNumber: '2',
+    strLength: 1,
+    strLengthPositive: true,
+    sum: 3,
+    numbersArray: [1, 2, 1, 3],
+    arrLength: 4,
+  })
+})
+
+test('8 params sync, mock', async () => {
+  const fn = klubok(
+    pure('incNumber', (ctx: { number: number }) => ctx.number + 1),
+    pure('strNumber', ({ incNumber }) => incNumber.toString()),
+    pure('strLength', ({ strNumber }) => strNumber.length),
+    pure('strLengthPositive', ({ strLength }) => strLength > 0),
+    pure('sum', ({ strLength, incNumber }) => strLength + incNumber),
+    pure('numbersArray', ({ number, incNumber, strLength, sum }) => [number, incNumber, strLength, sum]),
+    pure('arrLength', ({ numbersArray }) => numbersArray.length),
+    pure('isMocked', ({ numbersArray }) => numbersArray.length === 0)
+  )
+  assert.deepStrictEqual(await fn({ number: 1, numbersArray: [] }), {
+    number: 1,
+    incNumber: 2,
+    strNumber: '2',
+    strLength: 1,
+    strLengthPositive: true,
+    sum: 3,
+    numbersArray: [],
+    arrLength: 0,
+    isMocked: true,
+  })
+})
+
+test('9 params sync, only', async () => {
+  const fn = klubok(
+    pure('incNumber', (ctx: { number: number }) => ctx.number + 1),
+    pure('strNumber', ({ incNumber }) => incNumber.toString()),
+    pure('strLength', ({ strNumber }) => strNumber.length),
+    pure('strLengthPositive', ({ strLength }) => strLength > 0),
+    pure('sum', ({ strLength, incNumber }) => strLength + incNumber),
+    pure('numbersArray', ({ number, incNumber, strLength, sum }) => [number, incNumber, strLength, sum]),
+    pure('arrLength', ({ numbersArray }) => numbersArray.length),
+    pure('isMocked', ({ numbersArray }) => numbersArray.length === 0),
+    pure('neverExists', () => null)
+  )
+  assert.deepStrictEqual(await fn({ number: 1 }, new Set(['strLength', 'strNumber', 'incNumber'])), {
+    number: 1,
+    incNumber: 2,
+    strNumber: '2',
+    strLength: 1,
+  })
+})
+
+test('10 params sync, resp', async () => {
+  const fn = klubok(
+    pure('incNumber', (ctx: { number: number }) => ctx.number + 1),
+    pure('strNumber', ({ incNumber }) => incNumber.toString()),
+    pure('strLength', ({ strNumber }) => strNumber.length),
+    pure('strLengthPositive', ({ strLength }) => strLength > 0),
+    pure('sum', ({ strLength, incNumber }) => strLength + incNumber),
+    pure('numbersArray', ({ number, incNumber, strLength, sum }) => [number, incNumber, strLength, sum]),
+    pure('arrLength', ({ numbersArray }) => numbersArray.length),
+    pure('isMocked', ({ numbersArray }) => numbersArray.length === 0),
+    pure('nullHere', () => null),
+    pure('voidHere', () => void 0)
+  )
+  assert.deepStrictEqual(await fn({ number: 1 }), {
+    number: 1,
+    incNumber: 2,
+    strNumber: '2',
+    strLength: 1,
+    strLengthPositive: true,
+    sum: 3,
+    numbersArray: [1, 2, 1, 3],
+    arrLength: 4,
+    isMocked: false,
+    nullHere: null,
+    voidHere: void 0,
   })
 })
